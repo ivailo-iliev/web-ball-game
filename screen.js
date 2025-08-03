fb88f411ba0a2e40919f577307408cf9d1a93979
ivailo-iliev <ivailo.iliev@gmail.com>
2025-08-03 08:09:03 +0300

    Fix emoji pick function

--- screen.js
+++ screen.js
@@ -6,7 +6,7 @@ window.$ = sel => domCache[sel] || (domCache[sel] = document.querySelector(sel))
 
 window.u = Object.freeze({
   rand   : n      => Math.random() * n,
-  pick   : arr    => Math.floor(Math.random() * arr.length),
+  pick   : arr    => arr[Math.floor(Math.random() * arr.length)],
   between: (a, b) => a + Math.random() * (b - a),
   clamp  : (n,l,h)=> n < l ? l : n > h ? h : n
 });
